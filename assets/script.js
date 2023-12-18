87d031fc03497e7f8aeb661897b6ec7b62c0441d
CestSamual <mvf4@hotmail.co.uk>
2023-12-18 10:24:31 +0000

    add save functionality

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,12 @@ function displayTime() {
     timeBanner.text(rightNow);
   }
   displayTime();
+
+  $(document).ready(function() {
+    $(".saveBtn").on("click", function(){
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(text, time);
+  })
+})
+
