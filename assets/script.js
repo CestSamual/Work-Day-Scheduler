3244a511d7dc7375deb176ec6bdd672c9ef65347
CestSamual <mvf4@hotmail.co.uk>
2023-12-18 11:38:41 +0000

    Add timeblock colour coding

--- assets/script.js
+++ assets/script.js
@@ -12,5 +12,36 @@ function displayTime() {
         var time = $(this).parent().attr("id");
         localStorage.setItem(text, time);
   })
+
+  function getTime() {
+    var now = dayjs().hour();
+
+    $(".time-block").each(function(){
+      var select = $(this);
+      var id = select.attr('id');
+      var timeId = id.split("m")
+      var nowBlock = parseInt(timeId[1]);
+      var nowFormat = now +"00";
+     
+      if (nowBlock == nowFormat){
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+      } else if (nowBlock > nowFormat){
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+       }else {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        }
+    })    
+  }
+
+getTime();
+
+
+
 })
 
